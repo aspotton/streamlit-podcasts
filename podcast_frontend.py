6cb76c83cd510c999656a7c1dcddb9e63f74d835
Adam Spotton <aspotton@gmail.com>
2023-08-15 19:37:00 -0400

    Update podcast_frontend.py
    Experiment: add RSS to feed list

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -6,14 +6,15 @@ import os
 def main():
     st.title("Newsletter Dashboard")
 
-    available_podcast_info = create_dict_from_json_files('.')
-
     # Left section - Input fields
     st.sidebar.header("Podcast RSS Feeds")
 
     # Dropdown box
+    st.session_state.available_podcast_info = st.session_state.available_podcast_info or create_dict_from_json_files('.')
+    st.session_state.podcast_names = st.session_state.available_podcast_info.keys()
+    
     st.sidebar.subheader("Available Podcasts Feeds")
-    selected_podcast = st.sidebar.selectbox("Select Podcast", options=available_podcast_info.keys())
+    selected_podcast = st.sidebar.selectbox("Select Podcast", options=st.session_state.podcast_names, key='available_podcasts')
 
     if selected_podcast:
 
@@ -63,37 +64,9 @@ def main():
         # Call the function to process the URLs and retrieve podcast guest information
         podcast_info = process_podcast_info(url)
 
-        # Right section - Newsletter content
-        st.header("Newsletter Content")
-
-        # Display the podcast title
-        st.subheader("Episode Title")
-        st.write(podcast_info['podcast_details']['episode_title'])
-
-        # Display the podcast summary and the cover image in a side-by-side layout
-        col1, col2 = st.columns([7, 3])
-
-        with col1:
-            # Display the podcast episode summary
-            st.subheader("Podcast Episode Summary")
-            st.write(podcast_info['podcast_summary'])
-
-        with col2:
-            st.image(podcast_info['podcast_details']['episode_image'], caption="Podcast Cover", width=300, use_column_width=True)
-
-        # Display the podcast guest and their details in a side-by-side layout
-        col3, col4 = st.columns([3, 7])
-
-        with col3:
-            st.subheader("Podcast Guest")
-            st.write(podcast_info['podcast_guest'])
-
-        # Display the five key moments
-        st.subheader("Key Moments")
-        key_moments = podcast_info['podcast_highlights']
-        for moment in key_moments.split('\n'):
-            st.markdown(
-                f"<p style='margin-bottom: 5px;'>{moment}</p>", unsafe_allow_html=True)
+        podcast_name = podcast_info['podcast_details']['podcast_title']
+        # Process the file data as needed
+        st.session_state.available_podcast_info[podcast_name] = podcast_info
 
 def create_dict_from_json_files(folder_path):
     json_files = [f for f in os.listdir(folder_path) if f.endswith('.json')]
