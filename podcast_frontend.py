0bd9b469d53ac208fdced42db07453398bdf1388
Adam Spotton <aspotton@gmail.com>
2023-08-15 20:49:18 -0400

    Update podcast_frontend.py
    State changes

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -27,7 +27,7 @@ def main():
         st.session_state.available_podcast_info[podcast_name] = podcast_info
         st.session_state.podcast_names = list(st.session_state.available_podcast_info)
 
-        if not 'available_podcasts' in st:
+        if not 'available_podcasts' in st.session_state:
             st.available_podcasts = podcast_name
         st.available_podcasts = podcast_name
 
